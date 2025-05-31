712e8ee22d4b0706a20d28fb16c5837c686a6840
Kenjin32icon <kariukilewis04@gmail.com>
2025-05-31 13:49:21 +0300

    version1 of grade calculator which is wrong

--- src/classasigmnts/GradeCalculator.java
+++ src/classasigmnts/GradeCalculator.java
@@ -7,47 +7,66 @@ public class GradeCalculator {
 
         System.out.println("Simple Grade Calculator");
 
-        System.out.println("Given that Grade(A =>70, B=>60, C=>50, D=> 40 Fail<40): ");
-        char grade = scanner.next().charAt(0);
+        System.out.println("Given that Grade(A =>70, B=>60, C=>50, D=> 40 Fail<40)");
 
-
-        //Subject 1
-        //Subject input
-        System.out.print("Enter Subject 1: ");
-        String subject = scanner.next();
-
-        //subject repective Grade
-        System.out.print("Enter Subjects 1 Marks : ");
-        float marks = scanner.nextFloat();
-
-//        //Subject 2
+        // my version 1
+//        //Subject 1
 //        //Subject input
-//        System.out.print("Enter Subject 2: ");
-//        String subject2 = scanner.next();
+//        System.out.print("Enter Subject 1: ");
+//        String subject = scanner.next();
 //
 //        //subject repective Grade
-//        System.out.print("Enter Subjects 2 Marks : ");
-//        float grade2 = scanner.nextFloat();
-
-        float result = 0;
-        boolean validOperation = true;
-
-//        switch (grade) {
-//            case marks:
-//            if (marks >= 70) {
-//                System.out.println("Grade A ");
-//            } else if (marks >= 60 && marks < 69) {
-//                System.out.println("Grade B");
-//            } else if (marks >= 50 && marks < 59) {
-//                System.out.println("Grade C");
-//            } else if (marks >= 40 && marks < 49) {
+//        System.out.print("Enter Subjects 1 Marks : ");
+//        float marks = scanner.nextFloat();
+//
+////        //Subject 2
+////        //Subject input
+////        System.out.print("Enter Subject 2: ");
+////        String subject2 = scanner.next();
+////
+////        //subject repective Grade
+////        System.out.print("Enter Subjects 2 Marks : ");
+////        float grade2 = scanner.nextFloat();
+//
+//        char result = 0;
+//        boolean validOperation = true;
+//
+//        switch (marks) {
+//            case 'A' :
+//                if (marks >= 70);
+//                 System.out.println("Grade A ");
+//            break;
+//
+//            case 'B' :
+//                if (marks >= 60 && marks < 69);
+//                 System.out.println("Grade B");
+//            break;
+//
+//            case 'C' :
+//                if (marks >= 50 && marks < 59);
+//                 System.out.println("Grade C");
+//            break;
+//
+//            case 'D':
+//            if (marks >= 40 && marks < 49) {
 //                System.out.println("Grade D");
 //            } else {
 //                System.out.println("Grade F");
 //            }
+//                break;
+//
 //            default:
 //                System.out.println("Error: Invalid operator entered.");
 //                validOperation = false;
+//
+//                 //Print result if operation was valid
+//                        if (validOperation) {
+//        System.out.println("Result: " + subject + " " + marks + " = " + result);
 //        }
+//        }
+//        scanner.close();
+
+        // Verson 2
+
     }
 }
