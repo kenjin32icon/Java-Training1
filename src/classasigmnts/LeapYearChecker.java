95ee2e278fdf199e1e425a311701abc7e8c0d0fb
Lewis Kariuki <161077623+kenjin32icon@users.noreply.github.com>
2025-05-31 11:11:52 +0300

    Merge pull request #1 from Kenjin32icon/main
    Update repo with files for polymorphysim and inheretance

--- src/classasigmnts/LeapYearChecker.java
+++ src/classasigmnts/LeapYearChecker.java
@@ -1,24 +1,25 @@
-// 4. Leap Year Checker
-//- Ask the user to enter a year and check whether it is a leap year.
-/*import java.util.Scanner;
+package  classasigmnts;
 
-public class LeapYearChecker {
+import java.util.Scanner; //imports the Scanner class from the java.util tool kit
+public class LeapYearChecker{
     public static void main(String[] args){
-        System.out.println("LeapyearCheker");
+    System.out.println("Enter an year: ");
 
-        Scanner scanner = new Scanner(System.in);
-        int year;
+    Scanner scanner = new Scanner(System.in);
+    int year;
 
-        try{
-            System.out.println("Enter year: ");
-            if (year % 4 == 0) {
-                System.out.println("The entered " + year + " is a Leap Year.");
-            }
+    try {
+        year = scanner.nextInt();
 
-            else {
-                System.out.println("The entered " + year + " is Odd.");
-            }
-
+        if (year % 4 == 0) {
+            System.out.println("The entered " + year + " is a leap year.");
+        } else {
+            System.out.println("The entered year" + year + "is not a leap year.");
         }
-
-    }*/
+    } catch (Exception e){
+            System.out.println("Invalid input! Please enter a valid year.");
+        } finally{
+            scanner.close();
+        }
+    }
+}
