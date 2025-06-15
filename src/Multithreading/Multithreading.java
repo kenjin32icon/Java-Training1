ac8a51b826bc6bab8b4b788304e5d3946ca5ea06
Kenjin32icon <kariukilewis@gmail.com>
2025-06-15 15:21:37 +0300

    For loop to creat 5 threads + 2 more my thing 2 threads

--- src/Multithreading/Multithreading.java
+++ src/Multithreading/Multithreading.java
@@ -1,15 +1,22 @@
 package Multithreading;
 
 // Multithreading enables a program to do multiple different things at the same time.
-// The ability to execute multipe different paths of code at the same time.
-// in java usually use only one path but using mutithreading you can bypass this.
+// The ability to execute multiple different paths of code at the same time.
+// in java usually use only one path but using multithreading you can bypass this.
 public class Multithreading {
     public static void main (String[] args){
 
-        //creating an object
-        Multithreadthing myThing = new Multithreadthing();
+        // TO create more 2 threads use a for loop
+        for( int i = 0; i < 5; i++){
+            //creating an object
+            Multithreadthing myThing = new Multithreadthing();
+            Multithreadthing myThing2 = new Multithreadthing();
 
-        myThing.start();
+//Using start instead run java will branch of a new thread.
+            // Using run it would complete the first thread then goes to the second thread
+            myThing.start();
+            myThing2.start(); // After adding the second myThing 2  it would the 2 different threads would be running at the same time.
+        }
 
     }
 }
