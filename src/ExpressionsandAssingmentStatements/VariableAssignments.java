1a52c1bc7cf62a487f7ad09c25ec98689801bc55
Kenjin32icon <kariukilewis@gmail.com>
2025-06-18 10:48:05 +0300

    Understanding Expressions and assingment statements

---
+++ src/ExpressionsandAssingmentStatements/VariableAssignments.java
@@ -0,0 +1,18 @@
+package ExpressionsandAssingmentStatements;
+
+public class VariableAssignments {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 2;
+        System.out.println(x);
+        System.out.println(y);
+        y = x;
+        x = 7;
+        System.out.println(x);
+        System.out.println(y);
+        x = 5;
+        y = 9;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
